40b2e3500d382c16e71a10e2e751e8cf81817a6e
Tweatflash <tweatflash@gmail.com>
2025-05-13 13:08:51 +0100

    finally

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,6 @@
-import { Bricolage_Grotesque } from "next/font/google";
-const bricolageGrotesque = Bricolage_Grotesque({
-  subsets: ["latin"], 
-  weight: ["400", "700"], // Choose your weights
-});
 export default function Home() {
   return (
-    <main className={`${bricolageGrotesque.className} w-full h-[100%] `}>
+    <main className={`w-full h-[100%] `}>
     </main>
   )
 }
